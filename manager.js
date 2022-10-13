674a6e601bf27e767b21b03653a3600dc41416cf
matt485 <113977874+Matt485@users.noreply.github.com>
2022-10-13 15:43:59 +0200

    Repeated question function

--- manager.js
+++ manager.js
@@ -1,11 +1,6 @@
 const readline = require('readline');
 const fs = require('fs');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 const read_file = () => {
     try {
         const data = fs.readFileSync('./test.json', 'utf8');
@@ -15,10 +10,25 @@ const read_file = () => {
     }
 };
 
-var prompt = "Welcome to your task manager, Press:\n1. to see all your tasks\n2. to add a task\n3. to delete a task\n4. to mark a task as done\n5. to Exit the task manager\n\n";
-
-rl.question(prompt, (answer) => {
-    // TODO: Log the answer in a database
-    console.log(`\nThank you for your valuable feedback: ${answer}`);
-    rl.close();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+
+
+var question = "Welcome to your task manager, Press:\n1. to see all your tasks\n2. to add a task\n3. to delete a task\n4. to mark a task as done\n5. to Exit the task manager\n\n";
+
+var main = function () {
+    rl.question(question, function (answer) {
+        switch (answer) {
+            case 'exit':
+                return rl.close();
+                break;
+            default:
+                console.log('Answer: "', answer, '"');
+        };
+        main(); //Calling this function again to ask new question
+    });
+};
+
+main();
