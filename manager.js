2f7497ebac633c6306fc2df4f9dce4d74b0f7758
matt485 <113977874+Matt485@users.noreply.github.com>
2022-10-13 16:10:30 +0200

    Add item addition boilerplate

--- manager.js
+++ manager.js
@@ -11,33 +11,42 @@ const read_file = () => {
 };
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
+
+var task = () => {
+    return JSON.parse(JSON.stringify({"name": "default", "completed": false}));
+};
 
 var main = function () {
     const question = "Welcome to your task manager, Press:\n1. to see all your tasks\n2. to add a task\n3. to delete a task\n4. to mark a task as done\n5. to Exit the task manager\n\n";
 
     rl.question(question, function (answer) {
         switch (answer) {
-            case 'exit':
-                return rl.close();
-                break;
             case "1":
                 const items = read_file();
+                console.log("\n");
                 items.forEach(item => {
                     console.log(`${item.name}`);
                 });
+                console.log("\n");
                 break;
             case "2":
+                console.log("==========================");
+                rl.question("add?  ", function (added_item) {
+                    console.log(added_item);
+                    rl.close();
+                });
                 break;
             case "3":
                 break;
             case "4":
                 break;
             case "5":
+                return rl.close();
                 break;
-  default:
+            default:
                 console.log('Answer: "', answer, '"');
         };
         main(); //Calling this function again to ask new question
