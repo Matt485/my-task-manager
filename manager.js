6ec0cac7c60ab038871cf99c9d82e27fbe01de54
matt485 <113977874+Matt485@users.noreply.github.com>
2022-10-13 19:09:34 +0200

    added functions

--- manager.js
+++ manager.js
@@ -23,6 +23,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const read_all_tasks = () => {
+    const items = read_file();
+    let count = 1;
+    console.log("\n");
+    items.forEach(item => {
+        if (item.completed) {
+            console.log(`[x]-${count}- ${item.name}`);
+        } else {
+            console.log(`[ ]-${count}- ${item.name}`);
+        }
+        count++;
+    });
+    console.log("========================");
+    console.log("\n");
+};
+
 var task = () => {
     return JSON.parse(JSON.stringify({"name": "default", "completed": false}));
 };
@@ -33,15 +49,9 @@ var main = function () {
     rl.question(question, function (answer) {
         switch (answer) {
             case "1":
-                const items = read_file();
-                console.log("\n");
-                items.forEach(item => {
-                    console.log(`${item.name}`);
-                });
-                console.log("\n");
-                break;
+                    read_all_tasks();
+                    break;
             case "2":
-                console.log("==========================");
                 rl.question("Which task would you like to add?\n", function (added_item) {
                     task = task();
                     task["name"] = added_item;
@@ -52,16 +62,25 @@ var main = function () {
                 });
                 break;
             case "3":
-                const items_delete = read_file();
-                console.log("\n");
-                let count = 1;
-                items_delete.forEach(item => {
-                    console.log(`${count} - ${item.name}`);
-                    count++;
+                read_all_tasks();
+                rl.question("Which task would you like to delete?\n", function (deleted_item_nbr) {
+                    const index = Number(deleted_item_nbr) - 1;
+                    let all_tasks = read_file();
+                    all_tasks[index]["name"] = "";
+                    let result = all_tasks.filter(object => object["name"] != "");
+                    write_data(result);
+                    rl.close();
                 });
-                console.log("\n");
                 break;
             case "4":
+                read_all_tasks();
+                rl.question("Which task would you like to mark as completed?", function (completed_item_nbr) {
+                    const index = Number(completed_item_nbr) - 1;
+                    let tasks_all = read_file();
+                    tasks_all[index]["completed"] = true;
+                    write_data(tasks_all);
+                    rl.close();
+                });
                 break;
             case "5":
                 return rl.close();
@@ -69,7 +88,7 @@ var main = function () {
             default:
                 console.log('Answer: "', answer, '"');
         };
-        main(); //Calling this function again to ask new question
+        main(); //Calling this function to ask again
     });
 };
 
